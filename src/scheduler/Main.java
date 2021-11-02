88dad84768e3e0b8d8384c3d9dd302ba8e7ffbf0
flowmar <omarimamdev@gmail.com>
2021-11-02 13:41:46 -0500

    Add ResultSet Scrolling

--- src/scheduler/Main.java
+++ src/scheduler/Main.java
@@ -10,8 +10,12 @@ import tools.DBQuery;
 import tools.JDBC;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main extends Application
 {
@@ -53,31 +57,66 @@ public static void main(String[] args) throws SQLException
         // Get the Statement Reference
         Statement statement = DBQuery.getStatement();
         
-        // SQL Insert Statement
-        String insertStatement = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Update," +
-                                     "Last_Updated_By) " +
-                                     "VALUES " +
-                                     "('US'," +
-                                     "'2021-11-1 00:00:00', 'admin', NOW(),'admin');";
         
-        // Execute Statement
-        statement.executeUpdate(insertStatement);
+        String selectStatement = "SELECT * FROM countries"; // SELECT statement
+        
+        statement.execute(selectStatement); // Execute statement
+        
+        ResultSet rs = statement.getResultSet();
+        
+        // Forward Scroll ResultSet
+        while (rs.next())
+        {
+            int       countryId   = rs.getInt("Country_ID");
+            String    countryName = rs.getString("Country");
+            LocalDate createDate  = rs.getDate("Create_Date").toLocalDate();
+            LocalTime time        = rs.getTime("Create_Date").toLocalTime();
+            String createdBy = rs.getString("Created_By");
+            LocalDateTime lastUpdate = rs.getTimestamp("Last_Update").toLocalDateTime();
+            
+            // Display
+            System.out.println( countryId + " | " + countryName + " | " + createDate + " |" + time + " |" + createdBy + " |" + lastUpdate);
+        }
+        
+        
+        // SQL Insert Statement
+//        String insertStatement = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Update," +
+//                                     "Last_Updated_By) " +
+//                                     "VALUES " +
+//                                     "('US'," +
+//                                     "'2021-11-1 00:00:00', 'admin', NOW(),'admin');"; // Insert Statement
+        
+        // Variable Insert
+//        String countryName = "Canada";
+//        String createDate = "2020-11-2 00:00:00";
+//        String createdBy = "admin";
+//        String lastUpdate = "admin";
+//
+//        String insertStatement = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Update, " +
+//                                     "Last_Updated_By) VALUES(" +
+//                                     "'" + countryName + "'," +
+//                                     "'" + createDate + "'," +
+//                                     "'" + createdBy + "'," +
+//                                     "'" + lastUpdate + "')";
+//        // Execute Statement
+//        statement.execute(insertStatement);
         
         // Rows affected Confirmation
         if ( statement != null )
         {
             if(statement.getUpdateCount() > 0)
             {
-                System.out.println(statement.getUpdateCount() + " rows affected.");
+                System.out.println(statement.getUpdateCount() + " row(s) affected.");
             }
             else
             {
                 System.out.println("No changes occurred.");
             }
         }
-    
-        connection.close();
         
         launch(args);
+    
+        connection.close();
+    
     }
 }
